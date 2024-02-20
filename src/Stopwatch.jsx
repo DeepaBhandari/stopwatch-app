d0e9966074cd5455687adb0115e55bf8889348ee
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-02-20 20:07:01 +0545

    initialization of stopwatch app via vite

---
+++ src/Stopwatch.jsx
@@ -0,0 +1,9 @@
+import React,{ useState, useEffect, useRef } from "react";
+
+function Stopwatch(){
+    const [isRunning, setIsRunning] = useState(false);
+return(
+    <></>
+)
+}
+export default Stopwatch;
